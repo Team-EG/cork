cd3552834689be4ae6e4a9b25a7caddd84d6197a
eunwoo1104 <sions04@naver.com>
2020-12-28 11:06:07 +0900

    몇가지 처리

--- cogs/events.py
+++ cogs/events.py
@@ -29,6 +29,9 @@ class Events(commands.Cog):
     @commands.Cog.listener()
     async def on_command_error(self, ctx: commands.Context, error):
         if isinstance(error, commands.CommandNotFound):
+            # if ctx.author.id not in [288302173912170497, 558323117802389514]:
+            await ctx.send("잠시만요! 이 봇은 슬래시 커맨드로 작동하는 봇이에요. `/`을 입력해보세요.\n"
+                           "혹시 슬래시 커맨드가 등록되지 않았나요? 한번 봇을 다시 초대해보고 만약에 실패했다면 eunwoo1104#9600으로 문의해주세요.")
             return
         else:
             str_ex = ''.join(traceback.format_exception(type(error), error, error.__traceback__))
