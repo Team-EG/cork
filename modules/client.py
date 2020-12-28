85a69d96f87ccbe8699e1c8c5c556f1da86fd326
eunwoo1104 <sions04@naver.com>
2020-12-28 20:41:53 +0900

    대충 자동 개발/공개 토큰 선택

--- modules/client.py
+++ modules/client.py
@@ -10,10 +10,14 @@ from discord_slash import SlashCommand
 class CorkClient(commands.Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.debug = self.get_settings("debug")
         self.slash = SlashCommand(self, auto_register=True)
         self.db = SQLiteDB("main")
         self.logger = logging.getLogger("cork")
-        self.koreanbots = koreanbots.Client(self, self.get_settings("kor_token"))
+        self.koreanbots = koreanbots.Client(self, self.get_settings("kor_token"), postCount=not self.debug)
+
+    def run_bot(self):
+        self.run(self.get_settings("canary_token" if self.debug else "token"))
 
     @staticmethod
     def get_settings(key: str):
